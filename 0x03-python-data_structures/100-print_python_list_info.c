8905567d8a237b4f91cf66b02810e7c072b00aa6
DTAJ095 <albanjaures26@gmail.com>
2023-11-04 10:19:03 -0700

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,28 @@
+#include <sdtio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - Prints some basic info about Python lists
+ * @p: object
+ *
+ * Return: Nothing
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	long int list_size, allocate, i;
+
+	i = 0;
+	PyObject *object;
+
+	list_size = PyList_Size(p);
+	allocate = ((PyObject *p))->allocated;
+	printf("[*] Size of Python List = %ld\n", list_size);
+	printf("[*] Allocated = %ld\n", allocate);
+	while (i < list_size)
+	{
+		object = PyList_GetItem(p, i);
+		printf("Element %ld: %\n", i, Py_TYPE(object)->tp_name);
+		i++;
+	}
+}

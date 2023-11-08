b0dc8fa379b6c36ecfb04d5c5db67e608c2d3262
DTAJ095 <albanjaures26@gmail.com>
2023-11-08 10:23:12 -0800

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary.keys():
+        a_dictionary = value
+    else:
+        a_dictionary = value
+    return (a_dictionary)

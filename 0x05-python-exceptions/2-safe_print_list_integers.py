5bc7a9d3335eae970cb2b20a428e23bea604b56a
DTAJ095 <albanjaures26@gmail.com>
2023-11-20 08:29:13 -0800

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    nb_element = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            nb_element += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+
+    return (nb_element)

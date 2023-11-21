1ead58333a4d9d480ef7b625251c979cdc41e4b9
DTAJ095 <albanjaures26@gmail.com>
2023-11-21 05:56:00 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""define a class square"""
+
+class Square:
+    """square data"""
+
+    def __init__(self, size):
+        """Initialize a new square
+        Args:
+            size(int): the size of the square
+        """
+        self.__size = size

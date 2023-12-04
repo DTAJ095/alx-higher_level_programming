b0772db405f32fd9ab11aaf60a9fab4f1ea86911
DTAJ095 <albanjaures26@gmail.com>
2023-12-04 09:43:11 -0800

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" Defines BaseGeometry class """
+
+
+class BaseGeometry:
+    """ class attributes """
+    def area(self):
+        """ Returns the area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validates the value """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be grater than 0".format(name))

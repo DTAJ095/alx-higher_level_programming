b5950f196005c1c0db0c58ad901962b3c0845d8e
DTAJ095 <albanjaures26@gmail.com>
2023-12-05 09:11:35 -0800

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -11,9 +11,9 @@ class Student:
     """
     def __init__(self, first_name, last_name, age):
         """ initialize a new student """
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """ Returns a dictionary representation of Student

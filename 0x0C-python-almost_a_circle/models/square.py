5bd271d654b1230e9b551ce2ce39f2a748ce3e37
DTAJ095 <albanjaures26@gmail.com>
2023-12-11 06:56:43 +0100

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,35 +30,35 @@ class Square(Rectangle):
                 - 4th argument represents y attribute
             **kwargs (dict): New key/value pairs of attributes.
         """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.size = arg
-                elif a == 2:
-                    self.x = arg
-                elif a == 3:
-                    self.y = arg
-                a += 1
+         if args and len(args) != 0:
+             a = 0
+             for arg in args:
+                 if a == 0:
+                     if arg is None:
+                         self.__init__(self.size, self.x, self.y)
+                     else:
+                         self.id = arg
+                 elif a == 1:
+                     self.size = arg
+                 elif a == 2:
+                     self.x = arg
+                 elif a == 3:
+                     self.y = arg
+                 a += 1
 
-        elif kwargs and len(kwargs) != 0:
-            for key, val in kwargs.items():
-                if key == "id":
-                    if val is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = val
-                elif key == "size":
-                    self.size = val
-                elif key == "x":
-                    self.x = val
-                elif key == "y":
-                    self.y = val
+         elif kwargs and len(kwargs) != 0:
+             for key, val in kwargs.items():
+                 if key == "id":
+                     if val is None:
+                         self.__init__(self.size, self.x, self.y)
+                     else:
+                         self.id = val
+                 elif key == "size":
+                     self.size = val
+                 elif key == "x":
+                     self.x = val
+                 elif key == "y":
+                     self.y = val
 
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""

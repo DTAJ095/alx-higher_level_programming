8df42095bab735bae7c6188768be7faa9a2ae4ce
DTAJ095 <albanjaures26@gmail.com>
2024-02-12 04:16:30 -0800

    javascript wram up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const counter = process.argv.legnht;
+if (counter === 2)
+	console.log('No argument');
+else if (counter === 3)
+	console.log('Argument found');
+else
+	console.log('Arguments found');

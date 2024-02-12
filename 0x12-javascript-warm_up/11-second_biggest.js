bed90af7c283b162005914467d3dfb6db1082914
DTAJ095 <albanjaures26@gmail.com>
2024-02-12 07:48:54 -0800

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const list = process.argv.map(Number)
-	.slice(2, process.argv.length)
-	.sort((a, b) => a - b);
-	console.log(list[list.length - 2]);
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(list[list.length - 2]);
 }

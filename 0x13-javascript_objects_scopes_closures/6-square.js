46e131e9b68c27fd2c49957d41d49ba7b1cf8f98
DTAJ095 <albanjaures26@gmail.com>
2024-02-13 22:31:48 -0800

    added

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,9 +3,10 @@ module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
       this.print();
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
-    }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+	  }
   }
 };

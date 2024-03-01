1d0571cbb1336a5dca995cb9f6b37c783be89ba3
DTAJ095 <albanjaures26@gmail.com>
2024-03-01 13:09:01 -0800

    added

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Python script that takes 2 arguments
+in order to solve this challenge.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = 'https://api.github.com/reppos/{}/{}/commits'.format(repo_name,
+                                                               owner_name)
+    session = requests.Session()
+    response = session.get(url)
+    res = response.json()
+    for i, commit in enumerate(res):
+        if i == 10:
+            break
+        if type(commit) is dict:
+            name = commit.get('commit').get('author').get('name')
+            print('{}: {}'.format(commit.get('sha'), name))

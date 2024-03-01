6ae3df617257e494843c3481a0c08c5dc521c0b5
DTAJ095 <albanjaures26@gmail.com>
2024-03-01 09:17:17 -0800

    added

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+""" Find the peak module """
 
 
 def find_peak(list_of_integers):
     """ Return the peak of a list of unsorted integers """
     arr = list_of_integers
+    size = len(arr)
+    start, end = 0, size - 1
 
     if arr == []:
         return None
-    length = len(arr)
-    start, end = 0, length - 1
     while start < end:
         mid = start + (end - start) // 2
         if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:

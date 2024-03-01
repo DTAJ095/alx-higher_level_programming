894976122be7f958e314add3f55724ffcfb39110
DTAJ095 <albanjaures26@gmail.com>
2024-03-01 12:18:07 -0800

    added

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    data = {'q': q}
+    response = requests.post(url, data=data)
+    try:
+        json_res = response.json()
+        if not json_res:
+            print('No result')
+        else:
+            _id = json.res.get('id')
+            _name = json_res.get('name')
+            print('[{}] {}'.format(_id, _name))
+    except ValueError as invalid_json:
+        print('Not a valid json')

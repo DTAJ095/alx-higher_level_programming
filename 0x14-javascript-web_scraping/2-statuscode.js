c12567e33d7ea63b3702d47f0c1a8e90c2bc9682
DTAJ095 <albanjaures26@gmail.com>
2024-03-26 12:32:40 -0700

    web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@
 const request = require('request');
 
 request(process.argv[2], function (_error, response) {
-	console.log('code:', response.statusCode); // print the response status code if a response was received
+  console.log('code:', response.statusCode); // print the response status code if a response was received
 });

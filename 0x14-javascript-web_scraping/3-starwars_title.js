e00b328652e7b8affb8431db8805f16682653f85
DTAJ095 <albanjaures26@gmail.com>
2024-03-26 12:46:47 -0700

    web scraping

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request')
+const starWarUrl = 'https://swapi-api.alx-tools.com/api/films/'.concat(process.argv[2]);
+
+request(starWarUrl, function (error, response, body) {
+	body = JSON.parse(body);
+	console.log(body.title);
+});

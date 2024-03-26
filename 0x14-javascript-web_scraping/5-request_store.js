51479390e73e5980747b0b0585645e12dd081ee1
DTAJ095 <albanjaures26@gmail.com>
2024-03-26 16:07:12 -0700

    web scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,10 +3,10 @@
 const request = require('request');
 const fs = require('fs');
 
-request(process.argv[2], function(_error, _response, body) {
-	fs.writeFile(process.argv[3], body, 'utf8', function (error) {
-		if (error) {
-			console.log(error);
-		}
-	});
+request(process.argv[2], function (_error, _response, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
 });

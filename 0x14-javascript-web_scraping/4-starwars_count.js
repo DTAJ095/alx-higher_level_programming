0b0e59e7270680d49dab2ab4284d25b242924407
DTAJ095 <albanjaures26@gmail.com>
2024-03-26 15:53:55 -0700

    web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,10 +7,10 @@ let count = 0;
 request(starWarUrl, function (error, response, body) {
   body = JSON.parse(body).results;
 
-  for (let i = 0; i < body.lenght; ++i) {
+  for (let i = 0; i < body.length; ++i) {
     const characters = body[i].characters;
 
-    for (let j = 0; j < characters.lenght; ++j) {
+    for (let j = 0; j < characters.length; ++j) {
       const character = characters[j];
       const characterId = character.split('/')[5];
 
